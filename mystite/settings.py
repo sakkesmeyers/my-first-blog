8ad67ccd993285ac16cfb00f354c7bd00aea454d
Sam <sakke.smeyers@gmail.com>
2018-08-02 19:19:22 +0200

    Modified templates to display posts from database.

--- mystite/settings.py
+++ mystite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '#_=ppfwvmtu*l08cm6d4avswl%lzfhwwvpggs@&gtc3g7ylwg9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com',
+'localhost']
 
 
 # Application definition
